fc84d4e8caf3c749c103bf671e776718988b977a
stipan <micromillioner@yahoo.com>
2020-04-07 09:17:42 +0200

    mnist test

--- modules/mapping/kmedoids.cpp
+++ modules/mapping/kmedoids.cpp
@@ -105,6 +105,7 @@ namespace kmedoids_details {
         std::vector<int>& assignments, std::vector<int>& sec_nearest)
     {
         T total = 0;
+		vector_print(seeds);
         for (int j = 0; j < assignments.size(); j++) {
 			std::cout << "cost 3" << std::endl;
             int mi = seeds[i];  // object id of medoid i
